61f046f108f6c9bc108b99fa8f39656812566424
gsemer <gsemertzakis@hotmail.com>
2026-07-19 18:22:42 +0300

    initialize redis client

--- app/core/container.py
+++ app/core/container.py
@@ -5,15 +5,16 @@ import logging
 import httpx
 from asyncio import Semaphore
 from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker
+from redis.asyncio import from_url as redis_from_url
 
 from app.core.config import Settings
 from app.core.logging import get_logger
 from app.infrastructure.database.session import create_engine_and_sessionmaker
 from app.infrastructure.external.swapi_client import HTTPXSWAPIClient
+from app.infrastructure.cache.redis_lock_provider import RedisLockProvider
 from app.services.character_service import CharacterServiceImpl
 from app.services.film_service import FilmServiceImpl
 from app.services.starship_service import StarshipServiceImpl
-
 
 class Container:
     """Initializes and owns every shared, long-lived object the app
@@ -41,6 +42,9 @@ class Container:
         self.sessionmaker: async_sessionmaker
         self.engine, self.sessionmaker = create_engine_and_sessionmaker(settings)
 
+        self.redis_client = redis_from_url(settings.redis_url)
+        self.lock_provider = RedisLockProvider(self.redis_client, self.logger)
+
         self.http_client = httpx.AsyncClient(timeout=settings.swapi_timeout_seconds)
         self.swapi_client = HTTPXSWAPIClient(
             http_client=self.http_client,
@@ -52,7 +56,7 @@ class Container:
         )
 
         self.character_service = CharacterServiceImpl(
-            self.sessionmaker, self.swapi_client, self.logger, settings.import_batch_size
+            self.sessionmaker, self.swapi_client, self.logger, self.lock_provider, settings.import_lock_ttl_seconds, settings.import_batch_size
         )
         self.film_service = FilmServiceImpl(
             self.sessionmaker, self.swapi_client, self.logger, settings.import_batch_size
